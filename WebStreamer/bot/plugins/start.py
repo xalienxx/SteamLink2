2c39832bd999afdc72fc7336da064242eebbe8d7
xalienxx <95128665+xalienxx@users.noreply.github.com>
2022-01-14 16:39:27 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -8,14 +8,4 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @StreamBot.on_message(filters.command(['start', 'help']))
 async def start(b, m):
-    await m.reply('Hi, Send me a file to get an instant stream link.',
-                  reply_markup=InlineKeyboardMarkup(
-                      [
-                          [
-                              InlineKeyboardButton(
-                                  f'{emoji.STAR} Source {emoji.STAR}',
-                                  url='https://github.com/EverythingSuckz/TG-FileStreamBot'
-                              )
-                          ]
-                      ]
-                  ))
+    await m.reply('Hi, Send me a file to get an instant stream link.')
